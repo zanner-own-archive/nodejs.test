2f8bdc4a2c7efa1c605c8168e4be5720e9d3af50
Sergej Kukharev <sergej.kucharev@gmail.com>
2015-01-21 15:33:01 +0200

    0.0.1
    URL для удобство вставил в код

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //#!/usr/bin/env node
 
 console.log('server starting ...');
+// http://nodeguide.ru/doc/
 var argv = require('optimist')
 	.usage('Usage: $0 -host [name] -port [num]')
 	.demand(['host', 'port'])
@@ -9,9 +10,13 @@ var argv = require('optimist')
 	.argv;
 console.log('server host: ' + argv.host + ':' + argv.port);
 
+// http://expressjs.com/api.html#app-settings
 var express = require('express');
 var app = express();
+// https://www.npmjs.com/package/ejs
 var ejs = require('ejs');
+// https://docs.nodejitsu.com/articles/file-system/how-to-read-files-in-nodejs
+// http://nodejs.org/api/fs.html#fs_fs_read_fd_buffer_offset_length_position_callback
 var fs = require('fs');
 
 app.get('/', function (req, res){
@@ -19,6 +24,8 @@ app.get('/', function (req, res){
 		fs.readFile('tpl/show.html', 'utf8', function (err, data) {
 			if (err) throw err;
 			res.send(data);
+// http://www.w3schools.com/html/html5_canvas.asp
+// http://habrahabr.ru/post/111308/
 			//console.log(data);
 		});
 	});
